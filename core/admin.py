c6e6dd0047093c41ff74843a9658acfdb9db50d8
JairTex <steixeirajair@gmail.com>
2022-08-05 09:34:45 -0300

    Testes ORM 1x1 e 1xM

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Carro, Chassi, Montadora
+
+
+@admin.register(Chassi)
+class ChassAdmin(admin.ModelAdmin):
+    list_display = ('numero',)
+
+@admin.register(Montadora)
+class MontadoraAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+
+@admin.register(Carro)
+class CarroAdmin(admin.ModelAdmin):
+    list_display = ('montadora', 'modelo', 'chassi', 'preco')
--- core/models.py
+++ core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Chassi(models.Model):
+    numero = models.CharField('Chassi', max_length=16, help_text='Informe 16 caracteres')
+
+    class Meta:
+        verbose_name = 'Chassi'
+        verbose_name_plural = 'Chassis'
+    
+    def __str__(self):
+        return self.numero
+
+class Carro(models.Model):
+    chassi = models.OneToOneField(Chassi, on_delete=models.CASCADE) #1 to 1
+    montadora = models.ForeignKey('Montadora', on_delete=models.CASCADE) #1 to m
+    modelo = models.CharField('Modelo', max_length=30, help_text='Informe o modelo do Carro')
+    preco = models.DecimalField('Preço', max_digits=8, decimal_places=2, help_text='Informe o preço do carro')
+
+    class Meta:
+        verbose_name = 'Carro'
+        verbose_name_plural = 'Carros'
+    
+    def __str__(self):
+        return f'{self.montadora} {self.modelo}'
+
+class Montadora(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+
+    class Meta:
+        verbose_name = 'Montadora'
+        verbose_name_plural = 'Montadoras'
+    
+    def __str__(self):
+        return self.nome

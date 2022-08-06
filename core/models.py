0d632a9ed43497041f3743cd67227af226f124b9
JairTex <steixeirajair@gmail.com>
2022-08-06 16:59:22 -0300

    Adição de uma função para definir montadora como Padrão, caso alguma seja removida, fazendo asim com que os cadastros dos carro snão sejam perdidos com tal exclusão.

--- core/models.py
+++ core/models.py
@@ -12,9 +12,22 @@ class Chassi(models.Model):
     def __str__(self):
         return self.numero
 
+class Montadora(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+
+    class Meta:
+        verbose_name = 'Montadora'
+        verbose_name_plural = 'Montadoras'
+    
+    def __str__(self):
+        return self.nome
+
+def set_default_montadora():
+    return Montadora.objects.get_or_create(nome='Padrão')[0] # (object, boolean)
+
 class Carro(models.Model):
     chassi = models.OneToOneField(Chassi, on_delete=models.CASCADE) # 1 to 1
-    montadora = models.ForeignKey('Montadora', on_delete=models.CASCADE) # 1 to m
+    montadora = models.ForeignKey('Montadora', on_delete=models.SET(set_default_montadora)) # 1 to m
     motoristas = models.ManyToManyField(get_user_model()) # N to M
     modelo = models.CharField('Modelo', max_length=30, help_text='Informe o modelo do Carro')
     preco = models.DecimalField('Preço', max_digits=8, decimal_places=2, help_text='Informe o preço do carro')
@@ -25,13 +38,3 @@ class Carro(models.Model):
     
     def __str__(self):
         return f'{self.montadora} {self.modelo}'
-
-class Montadora(models.Model):
-    nome = models.CharField('Nome', max_length=50)
-
-    class Meta:
-        verbose_name = 'Montadora'
-        verbose_name_plural = 'Montadoras'
-    
-    def __str__(self):
-        return self.nome
